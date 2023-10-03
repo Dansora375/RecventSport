bfe2afb769a9a571050fb490493bd039f365bf6b
Dansora375 <danisora375@gmail.com>
2023-10-02 21:30:51 -0500

    primer commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>Helo word</div>
+  )
+}
+
+export default App
---
+++ src/api/create_sport_event.ts
@@ -0,0 +1,16 @@
+const API = "http://localhost:3000"
+
+// interface SportEvent {
+//   title: string;
+//   description: string;
+
+// }
+
+// export const createSportEvent = (sportEvent) => 
+//   fetch(`${API}/task`,{
+//     method: "POST",
+//     body: JSON.stringify(sportEvent),
+//     headers:{
+//       'Content-Type:': 'aplication/json'
+//     } 
+//   })
