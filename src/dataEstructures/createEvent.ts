5ebdc4c1f0e9f41d029f97577af4b6109c2b9449
Dansora375 <danisora375@gmail.com>
2023-10-08 12:38:15 -0500

    cambio

--- src/dataEstructures/createEvent.ts
+++ src/dataEstructures/createEvent.ts
@@ -1,8 +1,11 @@
 import { faker } from '@faker-js/faker';
 import cola from './queue';
 import pila from './stack';
+import ColaGenerica from './queueArray';
+import PilaGenerica from './stackArray';
 
 interface evento {
+  id?:number,
   nombre:string;
   description: string;
   deporte: string;
@@ -13,16 +16,19 @@ interface evento {
 }
 const eventsNumeber=1000
 const queue = new cola <evento>()
+const queueArray = new ColaGenerica <evento>(eventsNumeber)
+const stackArray = new PilaGenerica <evento>(eventsNumeber)
 const stack = new pila <evento>()
 
 
 export  function pruebaAddInQueue():void{
   
   faker.seed(42)
-  console.time("tiempo")
+  console.time("tiempo pruebaAddInQueue")
   for (let i =0; i<eventsNumeber;i++){
 
     const event: evento= {
+      id: i,
       nombre:faker.person.firstName(),
       description:faker.lorem.lines({ min: 1, max: 3 }),
       deporte: faker.word.adjective(),
@@ -35,8 +41,33 @@ export  function pruebaAddInQueue():void{
       queue.enqueue(event)
       // console.log(event)
   }
-  console.timeEnd("tiempo")
+  console.timeEnd("tiempo pruebaAddInQueue")
 }
+
+
+export  function pruebaAddInQueueArray():void{
+  faker.seed(42)
+  console.time("tiempo pruebaAddInQueueArray")
+  for (let i =0; i<eventsNumeber;i++){
+
+    const event: evento= {
+      nombre:faker.person.firstName(),
+      description:faker.lorem.lines({ min: 1, max: 3 }),
+      deporte: faker.word.adjective(),
+      fechaHora :faker.date.anytime(),
+      ubicacion:faker.location.street(),
+      capacidaMax:faker.number.int({ min: 10, max: 100 }),
+      costo:faker.number.int({ min: 1000, max: 10000 })
+      }
+      
+      queueArray.enqueue(event)
+      // console.log(event)
+  }
+  console.timeEnd("tiempo pruebaAddInQueueArray")
+
+}
+
+
 
 export   function pruebaEliminarInQueue(): void{
   console.time("tiempo eliminar")
@@ -46,6 +77,16 @@ export   function pruebaEliminarInQueue(): void{
   }
   console.timeEnd("tiempo eliminar")
 }
+
+export   function pruebaEliminarInQueueArray(): void{
+  console.time("tiempo eliminar")
+  for (let i =0; i<eventsNumeber;i++){
+    queueArray.dequeue()
+    
+  }
+  console.timeEnd("tiempo eliminar")
+}
+
 
 export  function pruebaAddInStack():void{
   console.time("tiempo")
@@ -82,4 +123,8 @@ export   function pruebaEliminarInStack(): void{
 
 pruebaAddInQueue()
 pruebaEliminarInQueue()
+
+
+// pruebaAddInStack()
+// pruebaEliminarInStack()
 
